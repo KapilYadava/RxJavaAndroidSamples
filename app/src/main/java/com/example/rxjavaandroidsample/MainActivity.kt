e312ceacb0313e902afb445f2f05656c434b097b
kapil yadav <kapilyadava.isa@gmail.com>
2020-03-22 18:57:43 +0530

    use of CompositeDisposable

--- app/src/main/java/com/example/rxjavaandroidsample/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaandroidsample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.observers.DisposableObserver
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,9 +14,10 @@ class MainActivity : AppCompatActivity(){
 
     private lateinit var myObservable: Observable<String>
     private lateinit var myObserver: DisposableObserver<String>
+    private lateinit var myObserver2: DisposableObserver<String>
     private val greetings = "Hello RxAndroid"
     private val TAG = "MainActivity"
-    //private lateinit var disposable: Disposable
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +39,30 @@ class MainActivity : AppCompatActivity(){
             }
         }
 
+        myObserver2 = object: DisposableObserver<String>() {
+
+            override fun onNext(t: String?) {
+                Log.v(TAG, "onNext: $t")
+                textview1.text = t
+            }
+            override fun onError(e: Throwable?) {
+                Log.v(TAG, "onError: " + e.toString())
+            }
+            override fun onComplete() {
+                Log.v(TAG, "onComplete")
+            }
+        }
+
+        compositeDisposable.add(myObserver)
+        compositeDisposable.add(myObserver2)
         myObservable.subscribe(myObserver)
+        myObservable.subscribe(myObserver2)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        //disposable.dispose()
-        myObserver.dispose()
+        //myObserver.dispose()
+        //myObserver2.dispose()
+        compositeDisposable.clear()
     }
 }
